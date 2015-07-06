2525ad96c969c37c76577c9590166fe37f467ddd
Scott Cytacki <scytacki@concord.org>
2015-07-06 12:09:13 -0400

    add visual logging and add pause after write

--- www/js/index.js
+++ www/js/index.js
@@ -36,10 +36,20 @@ var app = {
         app.deviceList = document.getElementById("deviceList");
         app.refreshButton = document.getElementById("refreshButton");
         app.updateNamesButton = document.getElementById("updateNames");
+        app.logList = document.getElementById("logList");
         app.refreshDeviceList();
 
         app.refreshButton.addEventListener("click", app.refreshDeviceList);
         app.updateNamesButton.addEventListener("click", app.updateNames);
+    },
+    clearLog: function () {
+        app.logList.innerHTML = ''; // empties the list
+    },
+    log: function (message) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = message;
+        app.logList.appendChild(listItem);
+        console.log(message);
     },
     refreshDeviceList: function() {
 
@@ -48,33 +58,47 @@ var app = {
     },
     updateNames: function() {
         app.deviceIds = [];
+        app.clearLog();
         rfduino.list(function(devices) {
+          // should log to screen that we got list of devices
+          app.log("got list of devices");
           devices.forEach(function(device) {
             app.deviceIds.push(device.uuid);
           });
-          app.updateNames2();
+          app.connectToNextDevice();
         }, app.onError);
     },
-    updateNames2: function() {
+    connectToNextDevice: function() {
         if(app.deviceIds.length == 0){
-            return
+            // should log to screen that no devices found
+            app.log("no devices found");
+            return;
         }
         app.currentDeviceId = app.deviceIds.pop();
         rfduino.connect(app.currentDeviceId, function (){
-          setTimeout(app.updateName3, 500);
+          app.log("connected to: " + app.currentDeviceId);
+          setTimeout(app.writeNameToDevice, 100);
         }, app.onError);
     },
-    updateName3: function(){
-      console.log("updating: " + app.currentDeviceId);
+    writeNameToDevice: function(){
+      app.log("updating: " + app.currentDeviceId);
       var encoder = new TextEncoder("utf-8");
       var customName = app.currentDeviceId.slice(-5);
       var command = "n" + customName;
       rfduino.write(encoder.encode(command).buffer, function (){
-        console.log(" disconnecting");
-        rfduino.disconnect(function (){
-          setTimeout(app.updateNames2, 500);
-        }, app.onError);
+        // in this library write returns immediately but it might
+        // not be finished writting yet, so instead of disconnecting immediately,
+        // wait a bit
+        app.log("write returned");
+        setTimeout(disconnectAndContinue, 500);
       }, app.onError);
+    },
+    disconnectAndContinue: function () {
+        app.log("disconnecting");
+        rfduino.disconnect(function (){
+          // give a little bit of time to disconnect
+          setTimeout(app.connectToNextDevice, 10);
+        }, app.onError);
     },
     onDiscoverDevice: function(device) {
         var listItem = document.createElement('li'),
@@ -85,7 +109,7 @@ var app = {
         app.deviceList.appendChild(listItem);
     },
     onError: function(error) {
-        console.log(error);
+        app.log(error);
     }
 };
 
